e7a08d658256034de3436c84fa64ff608dcc1de8
Mahmoud <mahmoudbarmawi926@gmail.com>
2023-10-28 22:59:49 +0200

    sat_version

--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -3,6 +3,11 @@ const router=Router()
 import * as categoriesController from './controller/categories.controller.js'
 import fileUpload,{fileValidation} from '../../services/multer.js'
 router.get('/',categoriesController.getCategories)
+router.get('/active',categoriesController.getActiveCategories)
+
+router.get('/:id',categoriesController.getSpicificCategories)
+router.put('/:id',fileUpload(fileValidation.image).single('image'),categoriesController.updateCategories)
+
 router.post('/',fileUpload(fileValidation.image).single('image'),categoriesController.createCategory)
 
 export default router
--- src/modules/categories/controller/categories.controller.js
+++ src/modules/categories/controller/categories.controller.js
@@ -1,10 +1,17 @@
 import slugify from "slugify";
 import categoryModle from "../../../../DB/models/category.model.js";
 import cloudinary from '../../../services/cloudinary.js'
-export const getCategories = (req, res) => {
-    return res.json({ Message: "Categories" });
-}
 
+export const getCategories = async(req, res) => {
+    const category=await categoryModle.find()
+    return res.json({message:"success",category});
+
+}
+export const getSpicificCategories = async(req, res) => {
+    const {id}=  req.params;
+    return res.json({message:"success",id});
+
+}
 export const createCategory = async (req, res) => {
     const name = req.body.name.toLowerCase();
     if (await categoryModle.findOne({ name })) {
@@ -15,4 +22,35 @@ export const createCategory = async (req, res) => {
     })
     const cat = await categoryModle.create({ name, slug: slugify(name), image: { secure_url, public_id } })
     return res.status(201).json({ message: "success", cat })
+}
+export const updateCategories = async(req, res) => {
+    const cat=await categoryModle.findById(req.params.id)
+
+    if(!cat){
+        return res.json({message:`invalid category id" ${req.params.id}`})
+    }
+    if(req.body.name){
+        if(await categoryModle.findOne({name:req.body.name}).select('name')){
+            return res.json({Message:`category ${req.body.name} already exist`})
+        }
+        cat.name=req.body.name
+        cat.slug=slugify(req.body.name)
+    }
+    if(req.body.status){
+        cat.status=req.body.status
+    }
+    if(req.file){
+        const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path,
+        {folder:`${process.env.APP_NAME}/categories`})
+        await cloudinary.uploader.destroy(cat.image.public_id)
+        cat.image={secure_url,public_id}
+    }
+    await cat.save();
+    return res.json({message:"Success",cat})
+}
+
+export const getActiveCategories = async(req, res) => {
+    const cat=await categoryModle.find({status:'Active'}).select('name image')
+    return res.json({message:"success",cat})
+
 }
