6178f78e04f5ac63cb341ddb2aeca207ceef7e78
Mahmoud <mahmoudbarmawi926@gmail.com>
2023-11-11 22:50:35 +0200

    sat_ver2

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -6,5 +6,6 @@ const router=Router()
 router.post('/signup',fileUpload(fileValidation.image).single('image'),AuthController.signUp);
 router.post('/signIn',AuthController.signIn);
 router.get('/confirmEmail/:email',AuthController.confirmEmail);
-
+router.patch('/sendCode',AuthController.sendCode);
+router.patch('/forgotPassword',AuthController.forgotPssword)
 export default router;
--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -2,13 +2,14 @@ import userModle from "../../../../DB/models/user.model.js";
 import bcrypt from 'bcryptjs'
 import cloudinary from "../../../services/cloudinary.js";
 import jwt from "jsonwebtoken";
+import { customAlphabet, nanoid } from "nanoid";
 import { sendEmail } from "../../../services/email.js";
 
 export const signUp = async (req, res) => {
     const { userName, email, password } = req.body;
 
     const user = await userModle.findOne({ email });
-    
+
     if (user) {
         return res.status(409).json({ message: "Email alrady exist" })
     }
@@ -16,31 +17,28 @@ export const signUp = async (req, res) => {
     const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path, {
         folder: `${process.env.APP_NAME}/profile`
     })
-    const emailToken= jwt.sign({email},process.env.EMAILSCRET)
-    await sendEmail(email, "Confirm Email", `<a href='http://localhost:3000/auth/confirmEmail/${emailToken}'>Verify</a>`)
-    
+    const emailToken = jwt.sign({ email }, process.env.EMAILSCRET)
+    await sendEmail(email, "Confirm Email", `<a href='${req.protocol}://${req.headers.host}/auth/confirmEmail/${emailToken}'>Verify</a>`)
+
     const hashpassword = await bcrypt.hash(password, parseInt(process.env.SALT_ROUND));
-    
+
     const createUser = await userModle.create({ userName, email, password: hashpassword, image: { secure_url, public_id } });
-    
+
     return res.status(201).json({ Message: "Success", createUser });
 }
-
-
 export const confirmEmail = async (req, res) => {
-        const token=req.params.email; 
-        if(!token) console.log("error");
-        const decoded=jwt.verify(token,process.env.EMAILSCRET)
-        if(!decoded){
-            return res.json({Message:'Can not verfiy email... try agin :(): ♥'})
-        }
-        const user=await userModle.findOneAndUpdate({email:decoded.email,confirmEmail:false},{confirmEmail:true});
-        if(!user){
-            return res.json({message:"Something went wrong"})
-        }
-    return res.json({message:"Confirmed successfully"})
+    const token = req.params.email;
+    if (!token) console.log("error");
+    const decoded = jwt.verify(token, process.env.EMAILSCRET)
+    if (!decoded) {
+        return res.json({ Message: 'Can not verfiy email... try agin :(): ♥' })
+    }
+    const user = await userModle.findOneAndUpdate({ email: decoded.email, confirmEmail: false }, { confirmEmail: true });
+    if (!user) {
+        return res.json({ message: "Something went wrong" })
+    }
+    return res.json({ message: "Confirmed successfully" })
 }
-
 
 export const signIn = async (req, res) => {
     const { email, password } = req.body;
@@ -55,4 +53,33 @@ export const signIn = async (req, res) => {
     }
     const token = jwt.sign({ id: user._id, role: user.role, status: user.status }, process.env.LOGINSCret);
     return res.status(201).json({ Message: "Success", token });
-} 
+}
+
+export const sendCode = async (req, res) => {
+    const { email } = req.body;
+    let code = customAlphabet('123456789abcdzABCDZ', 4)
+    code = code();
+    const user = await userModle.findOneAndUpdate({ email }, { sendCode: code }, { new: true })
+    const html = `<h2> code is ${code}</h2>`
+    await sendEmail(email, "reset password", html)
+    return res.redirect(process.env.PAGE)
+}
+export const forgotPssword = async (req, res) => {
+    const { email, password, code } = req.body;
+    const user = await userModle.findOne({ email });
+    if (!user) {
+        return res.json({ message: "not register user" });
+    }
+    if(user.sendCode != code){
+        return res.json({message:"Invalid code"});
+    }
+    let match=await bcrypt.compare(password,user.password)
+    if(match){
+        return res.json({message:"same password"})
+    }
+    user.password=await bcrypt.hash(password,parseInt(process.env.SALT_ROUND))
+    user.sendCode=null;
+    await user.save();
+    return res.json({message:"success"});
+
+}
