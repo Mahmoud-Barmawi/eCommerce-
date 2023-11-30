d5aec5637075d3a881e6d6c96cffdb45208c8158
Mahmoud <mahmoudbarmawi926@gmail.com>
2023-11-30 21:52:56 +0200

    th_version1

--- src/modules/coupon/coupon.router.js
+++ src/modules/coupon/coupon.router.js
@@ -7,6 +7,6 @@ router.get('/', controllerCoupon.getCoupon)
 router.put('/:id', controllerCoupon.updateCoupon)
 router.patch('/softDelete/:id', controllerCoupon.softDelete)
 router.delete('/hardDelete/:id', controllerCoupon.hardDelete)
-// router.patch('/restore/:id', controllerCoupon.restore)
+router.patch('/restore/:id', controllerCoupon.restore)
 
 export default router
